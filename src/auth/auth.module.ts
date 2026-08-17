30cf73af017ce2833effdb34c9a64d3d776a4a53
sahan nimnaka <nimnakasahan6@gmail.com>
2026-08-17 13:58:11 +0530

    Remove admin JWT expiration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,11 +9,9 @@ import { RolesGuard } from './roles.guard';
   imports: [
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: {
-        expiresIn: '15m',
-      },
     }),
   ],
+  
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy, RolesGuard],
   exports: [AuthService],
